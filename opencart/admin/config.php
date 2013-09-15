9e38e91dd281b4c5dfcb1e109c1a9e71cf710c51
maxim <shmaliy.maxim@gmail.com>
2013-09-15 19:23:02 +0300

    5464

--- opencart/admin/config.php
+++ opencart/admin/config.php
@@ -1,26 +1,26 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://dev.ska.com.ua/admin/');
-define('HTTP_CATALOG', 'http://dev.ska.com.ua/');
-define('HTTP_IMAGE', 'http://dev.ska.com.ua/image/');
+define('HTTP_SERVER', 'http://ska.com.ua/admin/');
+define('HTTP_CATALOG', 'http://ska.com.ua/');
+define('HTTP_IMAGE', 'http://ska.com.ua/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://dev.ska.com.ua/admin/');
-define('HTTPS_CATALOG', 'http://dev.ska.com.ua/');
-define('HTTPS_IMAGE', 'http://dev.ska.com.ua/image/');
+define('HTTPS_SERVER', 'http://ska.com.ua/admin/');
+define('HTTPS_CATALOG', 'http://ska.com.ua/');
+define('HTTPS_IMAGE', 'http://ska.com.ua/image/');
 
 // DIR
-define('DIR_APPLICATION', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/admin/');
-define('DIR_SYSTEM', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/system/');
-define('DIR_DATABASE', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/system/database/');
-define('DIR_LANGUAGE', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/admin/language/');
-define('DIR_TEMPLATE', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/admin/view/template/');
-define('DIR_CONFIG', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/system/config/');
-define('DIR_IMAGE', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/image/');
-define('DIR_CACHE', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/system/cache/');
-define('DIR_DOWNLOAD', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/download/');
-define('DIR_LOGS', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/system/logs/');
-define('DIR_CATALOG', '/home/virtwww/w_dev-ska-com_87b8b5ee/http/catalog/');
+define('DIR_APPLICATION', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/admin/');
+define('DIR_SYSTEM', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/system/');
+define('DIR_DATABASE', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/system/database/');
+define('DIR_LANGUAGE', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/admin/language/');
+define('DIR_TEMPLATE', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/admin/view/template/');
+define('DIR_CONFIG', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/system/config/');
+define('DIR_IMAGE', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/image/');
+define('DIR_CACHE', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/system/cache/');
+define('DIR_DOWNLOAD', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/download/');
+define('DIR_LOGS', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/system/logs/');
+define('DIR_CATALOG', '/home/virtwww/w_canon-dp-ua_3b1884ab/http/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
--- opencart/catalog/view/theme/bigshop/template/common/footer.tpl
+++ opencart/catalog/view/theme/bigshop/template/common/footer.tpl
@@ -72,6 +72,31 @@
       <?php endif; ?>
     </ul>
   </div>
+  <div class="column">
+    <ul>
+      <li><!--bigmir)net TOP 100-->
+            <script type="text/javascript" language="javascript"><!--
+            bmN=navigator,bmD=document,bmD.cookie='b=b',i=0,bs=[],bm={v:16902377,s:16902377,t:11,c:bmD.cookie?1:0,n:Math.round((Math.random()* 1000000)),w:0};
+            for(var f=self;f!=f.parent;f=f.parent)bm.w++;
+            try{if(bmN.plugins&&bmN.mimeTypes.length&&(x=bmN.plugins['Shockwave Flash']))bm.m=parseInt(x.description.replace(/([a-zA-Z]|\s)+/,''));
+            else for(var f=3;f<20;f++)if(eval('new ActiveXObject("ShockwaveFlash.ShockwaveFlash.'+f+'")'))bm.m=f}catch(e){;}
+            try{bm.y=bmN.javaEnabled()?1:0}catch(e){;}
+            try{bmS=screen;bm.v^=bm.d=bmS.colorDepth||bmS.pixelDepth;bm.v^=bm.r=bmS.width}catch(e){;}
+            r=bmD.referrer.slice(7);if(r&&r.split('/')[0]!=window.location.host){bm.f=escape(r);bm.v^=r.length}
+            bm.v^=window.location.href.length;for(var x in bm) if(/^[vstcnwmydrf]$/.test(x)) bs[i++]=x+bm[x];
+            bmD.write('<a href="http://www.bigmir.net/" target="_blank" onClick="img=new Image();img.src="http://www.bigmir.net/?cl=16902377";"><img src="http://c.bigmir.net/?'+bs.join('&')+'"  width="88" height="31" border="0" alt="bigmir)net TOP 100" title="bigmir)net TOP 100"></a>');
+            //-->
+            </script>
+            <noscript>
+                <a href="http://www.bigmir.net/" target="_blank"><img src="http://c.bigmir.net/?v16902377&s16902377&t11" width="88" height="31" alt="bigmir)net TOP 100" title="bigmir)net TOP 100" border="0" /></a>
+            </noscript>
+            <!--bigmir)net TOP 100-->
+      </li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
 
 <div class="contact">
 <ul>
